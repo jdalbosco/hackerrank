eabe5dfc03bd15e0ca0578dbd588aef247fb0245
Julia Dal Bosco <juliadalbosco1@gmail.com>
2022-06-18 03:10:17 -0300

    Update preparation-kits/one-week/d4-recursive_digit_sum.py
    Co-authored-by: João Vitor de Camargo <camargo.dev@gmail.com>

--- preparation-kits/one-week/d4-recursive_digit_sum.py
+++ preparation-kits/one-week/d4-recursive_digit_sum.py
@@ -1,5 +1,5 @@
-def super_digit(number, repeat=1):
-    digits = [int(digit) for digit in list(number)]
+def super_digit(str_number, repeat=1):
+    digits = [int(digit) for digit in list(str_number)]
     sum_of_digits = sum(digits) * repeat
     
     return sum_of_digits if sum_of_digits < 10 else super_digit(str(sum_of_digits))
