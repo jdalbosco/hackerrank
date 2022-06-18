9a0279ec87b6c39595bf236156524e84d5949eba
Julia Dal Bosco <juliadalbosco1@gmail.com>
2022-06-18 03:46:30 -0300

    Update preparation-kits/one-week/d5-mock_test.py
    Co-authored-by: João Vitor de Camargo <camargo.dev@gmail.com>

--- preparation-kits/one-week/d5-mock_test.py
+++ preparation-kits/one-week/d5-mock_test.py
@@ -3,7 +3,7 @@ def pairs(diff, numbers_arr):
     pairs = 0
     
     for number in numbers_arr:
-        if number - diff in numbers_set:
+        if (number - diff) in numbers_set:
             pairs += 1
     
     return pairs
