5dd40da604db8bfb2ad54c29a37a50ac3689abed
Julia Dal Bosco <juliadalbosco1@gmail.com>
2022-06-18 03:44:10 -0300

    Update preparation-kits/one-week/d1-mini_max_sum.py
    Co-authored-by: João Vitor de Camargo <camargo.dev@gmail.com>

--- preparation-kits/one-week/d1-mini_max_sum.py
+++ preparation-kits/one-week/d1-mini_max_sum.py
@@ -1,9 +1,9 @@
-def mini_max_sum(numbers, n):
+def mini_max_sum(numbers, length):
     numbers.sort()
     
     def mini_sum():
         mini_sum = 0
-        for i in range(len(numbers) - n):
+        for i in range(len(numbers) - length):
             mini_sum += numbers[i]
         return mini_sum
             
